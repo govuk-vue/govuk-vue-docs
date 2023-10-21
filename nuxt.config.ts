1c6958f7a5a97a7ce893fca83e3fcea0218f5ef0
Matt Eason <github@matteason.co.uk>
2023-10-21 15:16:09 +0100

    Update opengraph/twitter meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,14 +7,15 @@ export default defineNuxtConfig({
     head: {
       link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
       meta: [
-        { property: 'og:image', content: '/assets/images/gv-opengraph.png'},
+        { property: 'og:image', content: 'https://govukvue.org//assets/images/gv-opengraph.png'},
         { property: 'og:image:alt', content: 'GOV.UK Vue'},
+        { property: 'og:description', content: 'Vue versions of every GOV.UK component'},
         { name: 'description', content: 'GOV.UK Vue provides Vue implementations of every component in the GOV.UK Design System.' },
         { name: 'twitter:card', content: 'summary' },
         { name: 'twitter:title', content: 'GOV.UK Vue' },
-        { name: 'twitter:description', content: 'Vue versions of every GOV.UK component'},
-        { name: 'twitter:image', content: '/assets/images/gv-twitter.png'},
-        { name: 'twitter:image:alt', content: 'GOV.UK Vue crown logo'}
+        { name: 'twitter:image', content: 'https://govukvue.org/assets/images/gv-twitter.png'},
+        { name: 'twitter:image:alt', content: 'GOV.UK Vue crown logo'},
+        { name: 'twitter:description', content: 'Vue versions of every GOV.UK component'}
       ],
       htmlAttrs: {
         class: 'govuk-template',
